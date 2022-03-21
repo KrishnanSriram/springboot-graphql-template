9ce8a923c39d6c58dcaf1a63e55bb4a3b390dfa9
Krishnan Sriram <krishnan.sriramrama@usbank.com>
2022-03-21 16:17:18 -0400

    First commit

---
+++ skeleton/src/main/java/com/example/${{values.java_package_name}}/controller/HelloWorldController.java
@@ -0,0 +1,16 @@
+package ${{values.java_package_name}}.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloWorldController {
+    @GetMapping()
+    ResponseEntity<String> sayHello() {
+        return new ResponseEntity("Hello, Welcome to Java SpringBoot", HttpStatus.OK);
+    }
+}
